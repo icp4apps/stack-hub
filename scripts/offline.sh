c18ad5b9833ca43749aaabc662e01eb2111c5771
Jarek Gawor <you@example.com>
2020-08-10 00:11:43 -0400

    initial code

---
+++ scripts/offline.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -e
+
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+base_dir=$(cd "${script_dir}/.." && pwd)
+
+build_dir="${base_dir}/build/appsody_stacks"
+
+image_push() {
+    local name=$@
+    echo "Pushing $name"
+    if [ "$USE_BUILDAH" == "true" ]; then
+        buildah push --tls-verify=false $name
+    else
+        docker push $name
+    fi
+}
+
+image_mirror() {
+    local file=$1
+    oc image mirror -f "$file" --insecure
+}
+
+# push nginx image
+if [ -f "$build_dir/image_list" ]; then
+    while read line
+    do
+        if [ "$line" != "" ]; then
+            image_push $line
+        fi
+    done < $build_dir/image_list
+fi
+
+# mirror stack images
+if [ -f "$build_dir/image-mapping.txt" ]; then
+    image_mirror "$build_dir/image-mapping.txt"
+fi 
+
+# deploy nginx container
+oc apply -f "$build_dir/openshift.yaml"
